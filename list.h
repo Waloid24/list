413a59954fe730bc62dc03e3d558280c39ceb246
Waloid24 <111529670+Waloid24@users.noreply.github.com>
2022-10-29 23:58:42 +0300

    Before work with pointers

--- list.h
+++ list.h
@@ -5,9 +5,17 @@
 
 #define STANDART_SIZE 20;
 #define LONG_LINE "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+#define LINE_ERROR "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+#define dumpline(command, ...) fprintf(graph, command __VA_ARGS__)
+#define print_html(command, ...) fprintf(graphicBuf, command __VA_ARGS__)
+
+#define LIST_OK(List, listInfo)\
+    validator (List, listInfo);
 
 typedef double elem_t;
 const double poison = NAN;
+
+static int times_push_html = 1;
 
 typedef struct {
     size_t prev;
@@ -25,27 +33,33 @@ typedef struct {
 
 } info_t;
 
+
 list_t * listConstruct (info_t * listInfo, size_t customCapacity);
 
-void listResize (list_t ** List, info_t * listInfo);
-void fillingNewFields (list_t * list, size_t start, size_t finish);
-void listDump (info_t info, list_t * List);
-void ListDestructor (list_t ** List, info_t * listInfo);
-void ListDeleteThisElem (list_t * List, info_t * listInfo, size_t num_of_count);
+void     listResize              (list_t ** List, info_t * listInfo);
+void     fillingNewFields        (list_t *  list, size_t   start,    size_t finish);
+void     listDump                (info_t *   info, list_t ** List);
+void     ListDestructor          (list_t ** List, info_t * listInfo);
+void     ListDeleteThisElem      (list_t *  List, info_t * listInfo, size_t num_of_count);
 
-size_t PushBack (list_t ** List, info_t * listInfo, elem_t newMemb);
-size_t PushFront (list_t ** List, info_t * listInfo, elem_t newMemb);
-size_t ListInsertBeforeThisNum (list_t ** List, info_t * listInfo, elem_t newMemb, size_t num_of_count);
-size_t ListInsertAfterThisNum (list_t ** List, info_t * listInfo, elem_t newMemb, size_t num_of_count);
+size_t   PushBack                (list_t ** List, info_t * listInfo, elem_t newMemb);
+size_t   PushFront               (list_t ** List, info_t * listInfo, elem_t newMemb);
+size_t   ListInsertBeforeThisNum (list_t ** List, info_t * listInfo, elem_t newMemb, size_t num_of_count);
+size_t   ListInsertAfterThisNum  (list_t ** List, info_t * listInfo, elem_t newMemb, size_t num_of_count);
 
-size_t AccessFirstElem (info_t * info);
-size_t AccessLastElem (info_t * info);
-size_t ListEmpty (info_t * info);
-size_t ListSize (info_t * info);
-size_t IndexAfterThisElem (list_t * List, info_t * info, size_t num_of_count);
-size_t IndexBeforeThisElem (list_t * List, info_t * listInfo, size_t num_of_count);
+size_t   AccessFirstElem         (info_t *  info);
+size_t   AccessLastElem          (info_t *  info);
+size_t   ListEmpty               (info_t *  info);
+size_t   ListSize                (info_t *  info);
+size_t   IndexAfterThisElem      (list_t *  List, info_t * info,     size_t num_of_count);
+size_t   IndexBeforeThisElem     (list_t *  List, info_t * listInfo, size_t num_of_count);
 
-elem_t FindElemLogNum (list_t * List, info_t * listInfo, size_t num_of_count);
-size_t FindElemByValue (list_t * List, info_t * listInfo, elem_t num_of_count);
+elem_t   FindElemLogNum          (list_t *  List, info_t * listInfo, size_t num_of_count);
+size_t   FindElemByValue         (list_t *  List, info_t * listInfo, elem_t num_of_count);
 
-list_t * ListSort (list_t ** List, info_t * listInfo);
+list_t * ListSort                (list_t * List, info_t  *  listInfo);
+
+void     Graphviz                (list_t ** List, info_t * listInfo);
+void     validator               (list_t *  List, info_t * listInfo);
+void     createHTMLfile          (int *     num);
+void     cmdLine                 (int       num);
--- main.cpp
+++ main.cpp
@@ -1,62 +1,38 @@
 #include "list.h"
 
-//#define CLEAR_LOGFILE
+#define CLEAR_LOGFILE
 
 int main (void)
 {
     info_t info = {};
     list_t * List = listConstruct (&info, 3); 
-    printf ("Ctor\n");
-    listDump (info, List);
-    printf ("info.size = %zu\n", info.size);
+    //printf ("Ctor\n");
+    //listDump (info, List);
+    //printf ("info.size = %zu\n", info.size);
     // printf ("1\n");
     PushFront (&List, &info, 31);
-    listDump (info, List);
+    //listDump (info, List);
 
     PushBack (&List, &info, 87);
-    listDump (info, List);
+    //listDump (info, List);
+
 
     PushFront (&List, &info, 65);
-    listDump (info, List);
+    //listDump (info, List);
 
     // PushBack (&List, &info, 7);
     // listDump (info, List);
 
     ListInsertBeforeThisNum (&List, &info, 50, 2);
-    listDump (info, List);
+    listDump (&info, &List);
+
 
     ListInsertAfterThisNum (&List, &info, 71, 4);
-    listDump (info, List);
+    listDump (&info, &List);
 
     PushFront (&List, &info, 23);
-    listDump (info, List);
+    listDump (&info, &List);
 
-    // PushBack (&List, &info, 15);
-    // listDump (info, List);
-
-    // ListDeleteThisElem (List, &info, 3);
-    // listDump (info, List);
-
-    // PushBack (&List, &info, 89);
-    // listDump (info, List);
-
-    // PushFront (&List, &info, 33);
-    // listDump (info, List);
-
-    // size_t a = FindElemByValue (List, &info, 8);
-    // printf ("a = %zu\n", a);
-
-    list_t * newList = ListSort (&List, &info);
-    listDump (info, newList);
-
-    // size_t ip = IndexAfterThisElem (&List, &info, 5);
-    // printf ("ip = %zu\n", ip);
-
-    // size_t ip1 = IndexBeforeThisElem (&List, &info, 4);
-    // printf ("ip1 = %zu\n", ip1);
-
-    // ListDestructor (&List, &info);
-    // listDump (info, List);
 }
 
 void     ListDestructor          (list_t ** List,     info_t * info)
@@ -84,7 +60,7 @@ list_t * listConstruct           (info_t *  listInfo, size_t   customCapacity)
     listInfo->capacity              = customCapacity;
     listInfo->freePlace             = 1;
     listInfo->size                  = 0;
-    printf ("Before in func --listConstruct--\n");
+    //printf ("Before in func --listConstruct--\n");
     fillingNewFields (list, listInfo->head, listInfo->capacity - 1);
 
     return list;
@@ -103,7 +79,7 @@ size_t   PushBack                (list_t ** List,     info_t * listInfo, elem_t 
     size_t firstMem = listInfo->head;
 
     listInfo->freePlace = (*List)[listInfo->freePlace].next;
-    printf ("freePlace = %zu\n", listInfo->freePlace);
+    //printf ("freePlace = %zu\n", listInfo->freePlace);
     
 
     if (listInfo->size == 1)
@@ -154,7 +130,7 @@ void     fillingNewFields        (list_t *  list,     size_t   start,    size_t 
     list[finish].data = poison;
 }
 
-void     listDump                (info_t    info,     list_t * List)
+void     listDump                (info_t *  info,     list_t ** List)
 {
     //printf ("4444444444444444444444\n");
     #ifdef CLEAR_LOGFILE
@@ -166,21 +142,22 @@ void     listDump                (info_t    info,     list_t * List)
     // printf ("start\n");
 
     FILE * log = fopen ("log.txt", "a");
+    MY_ASSERT (log == nullptr, "Unable to access the file");
 
     fprintf (log, "\n\nNEW START\n");
     fprintf (log, LONG_LINE);
-    fprintf (log, "info.size      = %zu\n", info.size);
+    fprintf (log, "info.size      = %zu\n", info->size);
     // printf ("in listDump: info.size = %zu\n", info.size);
-    fprintf (log, "info.capacity  = %zu\n", info.capacity);
-    fprintf (log, "info.freePlace = %zu\n", info.freePlace);
-    fprintf (log, "info.head      = %zu\n", info.head);
-    fprintf (log, "info.tail      = %zu\n\n", info.tail);
+    fprintf (log, "info.capacity  = %zu\n", info->capacity);
+    fprintf (log, "info.freePlace = %zu\n", info->freePlace);
+    fprintf (log, "info.head      = %zu\n", info->head);
+    fprintf (log, "info.tail      = %zu\n\n", info->tail);
 
     fprintf (log, "index = ");
 
     // printf ("111\n");
 
-    for (int i = 0; i < info.capacity; i++)
+    for (int i = 0; i < info->capacity; i++)
     {
         fprintf (log, "%4d ", i);
     }
@@ -189,36 +166,36 @@ void     listDump                (info_t    info,     list_t * List)
     // printf ("222\n");
 
     fprintf (log, "data  = ");
-    for (int i = 0; i < info.capacity; i++)
+    for (int i = 0; i < info->capacity; i++)
     {
-        fprintf (log, "%4.1lf ", List[i].data);
+        fprintf (log, "%4.1lf ", (*List)[i].data);
     }
     fprintf (log, "\n");
     
     // printf ("333\n");   
 
     fprintf (log, "next  = ");
-    for (int i = 0; i < info.capacity; i++)
+    for (int i = 0; i < info->capacity; i++)
     {
-        fprintf (log, "%4zu ", List[i].next);
+        fprintf (log, "%4zu ", (*List)[i].next);
     }
     fprintf (log, "\n");
 
     // printf ("444\n");
 
     fprintf (log, "prev  = ");
-    for (int i = 0; i < info.capacity; i++)
+    for (int i = 0; i < info->capacity; i++)
     {
-        fprintf (log, "%4zu ", List[i].prev);
+        fprintf (log, "%4ld ", (*List)[i].prev);
     }
     fprintf (log, "\n");
 
     //printf ("555\n");
 
-    fflush(log);
+    Graphviz (List, info);
 
+    fflush(log);
     fclose (log);
-    // printf ("666\n");
 }  
     
 size_t   PushFront               (list_t ** List,     info_t * listInfo, elem_t newMemb)
@@ -312,12 +289,12 @@ size_t   ListInsertAfterThisNum  (list_t ** List,     info_t * listInfo, elem_t 
             listResize (List, listInfo);
         }
 
-        printf ("num_of_count = %zu\n", num_of_count);
-        printf ("listInfo->size = %zu\n", listInfo->size);
+        //printf ("num_of_count = %zu\n", num_of_count);
+        //printf ("listInfo->size = %zu\n", listInfo->size);
 
         if (num_of_count >= listInfo->size)
         {
-            printf ("YYEESS\n");
+            //printf ("YYEESS\n");
             return PushBack(List, listInfo, newMemb);
         }
 
@@ -410,15 +387,30 @@ size_t   IndexBeforeThisElem     (list_t * List,      info_t * listInfo, size_t 
 void     ListDeleteThisElem      (list_t * List,      info_t * listInfo, size_t num_of_count)
 {
     MY_ASSERT (List == nullptr, "There is no access to list");
+    MY_ASSERT (listInfo == nullptr, "There is no access to information about list");
+
+    if (num_of_count >= listInfo->size)
+    {
+        num_of_count = listInfo->size;
+    }
 
     size_t indexThisNum = List[listInfo->head].next;
-    
+
     for (int i = 1; i < listInfo->size && i < num_of_count-1; i++)
     {
         indexThisNum = List[indexThisNum].next;
     }
     //printf ("this number is %lf\n", List[indexThisNum].data);
     
+    if (List[indexThisNum].prev == List[listInfo->head].prev) //There is the head of the list
+    {
+        listInfo->head = List[listInfo->head].next;
+    }
+    if (List[indexThisNum].prev == List[listInfo->tail].prev) //There is the tail of the list
+    {
+        listInfo->tail = List[listInfo->tail].prev;
+    }
+
     List[indexThisNum].data = NAN;
     List[indexThisNum].prev = -1;
     List[indexThisNum].next = listInfo->freePlace;
@@ -431,6 +423,7 @@ void     ListDeleteThisElem      (list_t * List,      info_t * listInfo, size_t 
 elem_t   FindElemLogNum          (list_t * List,      info_t * listInfo, size_t num_of_count)
 {
     MY_ASSERT (List == nullptr, "There is no access to list");
+    MY_ASSERT (listInfo == nullptr, "There is no access to information about list");
 
     size_t indexThisNum = List[listInfo->head].next;
     
@@ -445,6 +438,7 @@ elem_t   FindElemLogNum          (list_t * List,      info_t * listInfo, size_t 
 size_t   FindElemByValue         (list_t * List,      info_t * listInfo, elem_t num_of_count)
 {
     MY_ASSERT (List == nullptr, "There is no access to list");
+    MY_ASSERT (listInfo == nullptr, "There is no access to information about list");
     
     size_t indexThisNum = List[listInfo->head].next;
 
@@ -465,10 +459,11 @@ size_t   FindElemByValue         (list_t * List,      info_t * listInfo, elem_t 
     return -1;
 }
 
-list_t * ListSort (list_t ** List, info_t * listInfo)
+list_t * ListSort                (list_t ** List, info_t * listInfo)
 {
     MY_ASSERT (*List == nullptr, "There is no access to list");
     list_t * newList = (list_t *) calloc (listInfo->capacity, sizeof(list_t));
+    MY_ASSERT (newList == nullptr, "Unable to allocate memory");
 
     newList[0] = (*List)[0];
 
@@ -482,7 +477,7 @@ list_t * ListSort (list_t ** List, info_t * listInfo)
         newList[i].prev = i - 1;
     }
 
-    for (int i = listInfo->size+1; i < listInfo->capacity; i++)
+    for (int i = (listInfo->size)+1; i < listInfo->capacity; i++)
     {
         newList[i] = (*List)[i];
         newList[i].prev = -1;
@@ -499,11 +494,128 @@ list_t * ListSort (list_t ** List, info_t * listInfo)
     return newList;
 }
 
-void Graphviz (list * List, info_t * listInfo)
+void     Graphviz                (list_t ** List, info_t * listInfo)
 {
-    FILE * dumpGraph = fopen ("dumpGraphviz.dot", "w");
+    FILE * graph = fopen ("ListGraphviz.dot", "w");
+    MY_ASSERT (graph == nullptr, "There is no access to file with graph");
 
-    fprintf ("")
+    printf ("lol\n");
 
-    system ("dot -Tjpg dumpGraphviz.dot -o graph1.png");
+    *List = ListSort(List, listInfo);
+
+    printf ("after listSort");
+
+    dumpline("digraph test{\n");
+    dumpline("size =\"8.5\";\n");
+    dumpline("rankdir = LR;\n");
+
+    dumpline("node [shape=record];\n");
+
+    dumpline("  table_0 [style = filled, fillcolor=\"#fde0dd\", label=\"0|%.2lf|<n0>n: %zu|<p0>p: %zu\"];\n",, (*List)[0].data, (*List)[0].next, (*List)[0].prev);
+    size_t current = listInfo->head;
+    for (int i = 1; i <= listInfo->size; i++)
+    {
+        dumpline("  table_%zu [style = filled, fillcolor=\"#f0f0f0\", label=\"%d|%.2lf|<n%zu>n: %zu|<p%zu>p: %zu\"];\n",, (*List)[current].next, i, (*List)[current].data, (*List)[current].next, (*List)[current].next, (*List)[current].prev, (*List)[current].prev);
+        current = (*List)[current].next;
+    }
+
+    //dumpline("node [fillcolor=\"#fee8c8\"]");
+    dumpline("infoList [style = filled, fillcolor=\"#fee8c8\", label=\"capacity = %zu|size = %zu|head = %zu|tail = %zu\"];\n",, listInfo->capacity, listInfo->size, listInfo->head, listInfo->tail);
+
+    current = listInfo->head;
+    dumpline("  infoList->table_0 [style=invis];\n");
+    dumpline("  table_0:n0->table_%zu:n%zu [style=invis];\n",, (*List)[current].next, (*List)[current].next);
+    size_t nextBox = 0;
+    for (int i = 1; i < listInfo->size; i++)
+    {
+        nextBox = (*List)[current].next;
+        dumpline("  table_%zu:n%zu->table_%zu:n%zu;\n",, (*List)[current].next, (*List)[current].next, (*List)[nextBox].next, (*List)[nextBox].next);
+        current = (*List)[current].next;
+    }
+
+    dumpline("\n");
+
+    current = listInfo->tail;
+    nextBox = 0;
+    for (int i = 1; i < listInfo->size; i++)
+    {
+        nextBox = (*List)[current].prev;
+        dumpline("  table_%zu:p%zu->table_%zu:p%zu;\n",, (*List)[current].next, (*List)[current].prev, (*List)[nextBox].next, (*List)[nextBox].prev);
+        current = (*List)[current].prev;
+    }
+
+    dumpline("}\n\n");    
+
+    fflush (graph);
+    fclose (graph);
+
+    cmdLine(times_push_html);
+
+    createHTMLfile (&times_push_html);
+    
+}
+
+void     validator               (list_t * List, info_t * listInfo)
+{
+    size_t current = listInfo->head;
+    size_t nextMem = List[current].next;
+    size_t prevMem = List[current].prev;
+
+    for (int i = 0; i < listInfo->size; i++)
+    {
+        //printf ("in validator %d\n", i);
+        if (List[List[nextMem].prev].data == List[current].data && List[List[prevMem].next].data == List[current].data)
+        {
+            // printf (LONG_LINE);
+            // printf ("List[List[%zu].prev].data = %.2lf\n", nextMem, List[List[nextMem].prev].data);
+            // printf ("List[List[%zu].next].data = %.2lf\n", prevMem, List[List[prevMem].next].data);
+            // printf ("List[%zu].data = %.2lf\n", current, List[current].data);
+
+            current = List[current].next;
+            nextMem = List[current].next;
+            prevMem = List[current].prev;
+            // printf ("current = %zu\n", current);
+            // printf ("nextMem = %zu\n", nextMem);
+            // printf ("prevMem = %zu\n", prevMem);
+            // printf (LONG_LINE);
+            // printf ("\n");
+        }
+        else
+        {
+            FILE * log = fopen ("log.txt", "a");
+            fprintf (log, LINE_ERROR);
+            fprintf (log, "The element in logical number %zu has an incorrect value\n", current);
+            fprintf (log, "The current number is %.2lf\n", List[current].data);
+            fprintf (log, "The next element is %.2lf\n", List[List[prevMem].next].data);
+            fprintf (log, "The previous element is %.2lf\n", List[List[nextMem].prev].data);
+            fprintf (log, LINE_ERROR);
+
+            listDump (listInfo, &List);
+        }
+    }
+}
+
+void cmdLine (int num)
+{
+    char buf[100] = {};
+    sprintf(buf, "dot -T png -o graph%d.png ListGraphviz.dot", num);\
+    system(buf);
+}
+
+void createHTMLfile (int * num)
+{
+    FILE * graphicBuf = fopen("HTMLBuf.html", "a");
+    MY_ASSERT (graphicBuf == nullptr, "Unable to access to file");
+
+    char buf[60] = {};
+    sprintf (buf, "graph%d.png", *num);
+
+    print_html("<pre>\n");
+    print_html("<img src=\"%s\" alt=\"dump №%d\"/>\n",, buf, *num);
+    print_html("</pre>\n");
+
+    (*num)++;
+
+    fflush (graphicBuf);
+    fclose (graphicBuf);
 }
